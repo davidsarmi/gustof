1fff1dbb6a4453f313f2fb9bd40e3df799b23f38
Aprendiz <Aprendiz@SGLDFPCAT7F580.FormacionSena.RED>
2019-04-09 14:31:37 -0500

    cambios chef

--- gustof-frontend/src/views/Chef.vue
+++ gustof-frontend/src/views/Chef.vue
@@ -1,147 +1,130 @@
 
 <template v-slot:items="props">
-<div id="app">
-  <v-app id="inspire">
-
-    <v-toolbar app fixed clipped-left>
-      <img src="../assets/gustof.png">
-      <v-toolbar-title></v-toolbar-title>
-    </v-toolbar>
-    <v-menu
-      ref="menu"
-      v-model="menu"
-      :close-on-content-click="false"
-      :nudge-right="40"
-      lazy
-      transition="scale-transition"
-      offset-y
-      full-width
-      min-width="290px"
-    >
-      <template v-slot:activator="{ on }">
-        <v-text-field
+  <div id="app">
+    <v-app id="inspire">
+      <v-toolbar app fixed clipped-left>
+        <img src="../assets/gustof.png">
+        <v-toolbar-title></v-toolbar-title>
+      </v-toolbar>
+      <v-menu
+        ref="menu"
+        v-model="menu"
+        :close-on-content-click="false"
+        :nudge-right="40"
+        lazy
+        transition="scale-transition"
+        offset-y
+        full-width
+        min-width="290px"
+      >
+        <template v-slot:activator="{ on }">
+          <v-text-field v-model="date" label="fecha actual" readonly v-on="on"></v-text-field>
+        </template>
+        <v-date-picker
+          ref="picker"
           v-model="date"
-          label="fecha actual"
-          readonly
-          v-on="on"
-        ></v-text-field>
-      </template>
-      <v-date-picker
-        ref="picker"
-        v-model="date"
-        :max="new Date().toISOString().substr(0, 10)"
-        min="1950-01-01"
-        @change="save"
-      ></v-date-picker>
-    </v-menu>
-    <div id="appTareas">
-      <input type="text" placeholder="integrar tarea" v-model="txtTarea">
-      <button v-on:click="agregarTarea">agregar tarea</button>
-  <ul v-for=" tarea in listaTareas">
-    <li>
-      <input type="checkbox"  name="" id="" v-model="tarea.checked">
-      <label>{{tarea.texto}}</label>
-      <button v-on:click="EliminarTarea(tarea)">eliminar</button>
-    </li>
-  </ul>
-</div>
-  </v-app>
-</div>
-    </template>
+          :max="new Date().toISOString().substr(0, 10)"
+          min="1950-01-01"
+          @change="save"
+        ></v-date-picker>
+      </v-menu>
+      <div id="appTareas">
+        <input type="text" placeholder="integrar tarea" v-model="txtTareas">
+        <button v-on:click="agregarTarea">agregar tarea</button>
+        <ul>
+          <li v-for="(tarea , i) in listaTareas" :key="i">
+            <input type="checkbox" name id v-model="tarea.checked">
+            <label>{{tarea.texto}}</label>
+            <button v-on:click="EliminarTarea(tarea)">eliminar</button>
+          </li>
+        </ul>
+      </div>
+    </v-app>
+  </div>
+</template>
 
-<<<<<<< HEAD
 
 <script>
 export default {
-   el:"#appTareas",
-    data:{
-      txtTareas:"...",
-      listaTareas:[]
-    },
-methods: {
-  agregarTarea:function(){
-    var tarea=this.txtTareas.trim();
-    if(tarea){
-      this.listaTareas.push({
-        texto:tarea,
-        checked:false
-      });
-    }
-    this.txtTareas="";
-    alert("tarea agregada");
-    console.log(this.listaTareas);
+  el: "#appTareas",
+  data() {
+    return {
+      txtTareas: "...",
+      listaTareas: []
+    };
   },
-  EliminarTarea:function(){
-    var index= this.listaTareas.indexof(tarea);
-    this.listaTareas.splice(index,1);
+  methods: {
+    agregarTarea() {
+      var tarea = this.txtTareas;
+      if (tarea) {
+        this.listaTareas.push({
+          texto: tarea,
+          checked: false
+        });
+      }
+      this.txtTareas = "...";
+    },
+    EliminarTarea: function() {
+      var index = this.listaTareas.indexof(tarea);
+      this.listaTareas.splice(index, 1);
+    }
   }
-},
-=======
-<script>
-export default {
-  name: 'Chef',
-  created () {
-    this.$store.commit('SET_LAYOUT', 'administrador-layout')
-  }
->>>>>>> 4904719f1f2cca6522d75450f39c0bc537353142
-}
+};
 </script>
    
 
 
 
     <style scoped>
-    .v-toolbar__content > *:last-child, .v-toolbar__extension > *:last-child {
-    margin-right: 0;
-    color:white;
+.v-toolbar__content > *:last-child,
+.v-toolbar__extension > *:last-child {
+  margin-right: 0;
+  color: white;
 }
-.v-toolbar__content, .v-toolbar__extension {
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    align-items: center;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    padding: 0 24px;
-    background-color: black;
+.v-toolbar__content,
+.v-toolbar__extension {
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  padding: 0 24px;
+  background-color: black;
 }
 div.elevation-1 {
-        width: 1334px;
-        margin: 0px;
-        display: inline-block;
-
+  width: 1334px;
+  margin: 0px;
+  display: inline-block;
 }
 
-.botonesverdes{
-
-margin: -28px 0px 0 950px;
-
+.botonesverdes {
+  margin: -28px 0px 0 950px;
 }
 
 .theme--light.v-table {
-    background-color: #fff;
-    margin: 59px 0 0 0px;
-    color: rgba(0,0,0,0.87);
+  background-color: #fff;
+  margin: 59px 0 0 0px;
+  color: rgba(0, 0, 0, 0.87);
 }
 
- pre {
-    color: rgb(255, 255, 255);
-    font-family: 'Courier';
-    font-size: 18px;
-    text-align: justify;
-    text-decoration: none;
-    display: inline-block;
-    background-color: rgb(0, 0, 0);
-    padding: 15px 15px;
-    display: inline-block;
-       margin: 45px 150px 0px 150px ;
+pre {
+  color: rgb(255, 255, 255);
+  font-family: "Courier";
+  font-size: 18px;
+  text-align: justify;
+  text-decoration: none;
+  display: inline-block;
+  background-color: rgb(0, 0, 0);
+  padding: 15px 15px;
+  display: inline-block;
+  margin: 45px 150px 0px 150px;
 }
 
-  .v-toolbar{
-     background-color:black;
-  }
-  img{
-    width:150px;
-    
-  }
-   </style>
+.v-toolbar {
+  background-color: black;
+}
+img {
+  width: 150px;
+}
+</style>
